68caa7f46acbfcd22c9ff559471ec8eda7dfc510
mukil-gith <mukilmanimaran87@gmail.com>
2026-06-10 14:32:57 +0000

    made assingment of a and b in one line.

--- hello.java
+++ hello.java
@@ -4,9 +4,7 @@ public class hello
 {
     public static void main (String args [])
     {
-        int a,b;
-        a=10;
-        b=10;
+        int a=10,b=10;
         System.out.println("a="+a);
         System.out.println("b="+b);
     }
